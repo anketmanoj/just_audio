92735e05ca7bd467b5009af7d9a51750e8ef09b4
anketmanoj <68798781+anketmanoj@users.noreply.github.com>
2023-02-13 18:10:42 +0900

    now

--- just_audio_equalizer/macos/Classes/JustAudioPlugin.m
+++ just_audio_equalizer/macos/Classes/JustAudioPlugin.m
@@ -1,12 +1,12 @@
 #import "JustAudioPlugin.h"
-#if __has_include(<just_audio/just_audio-Swift.h>)
-#import <just_audio/just_audio-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+// #if __has_include(<just_audio/just_audio-Swift.h>)
+// #import <just_audio/just_audio-Swift.h>
+// #else
+// // Support project import fallback if the generated compatibility header
+// // is not copied when this plugin is created as a library.
+// // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
 #import "just_audio-Swift.h"
-#endif
+// #endif
 
 #import <AVFoundation/AVFoundation.h>
 
